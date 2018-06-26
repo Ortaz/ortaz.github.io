85c1ac3543f4a0e6d12e2341170043065bc1cb30
Ortaz <Natz94@mail.ru>
2018-06-26 09:37:11 +0300

    scd commit

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 require "./libs/rb-mysql.php";
 R::setup('mysql:host=sql23.main-hosting.eu;dbname=u742711990_domin',
         'u742711990_ortaz', 'TCQafYVLfpc5cx3AxN' ); //for both mysql or mariaDB
-if(!R::textConnection()){
+if(!R::testConnection()){
 	echo "Не удалось подключиться";
 	exit();	
 }
 echo "Подключился!";	
+function register_user($name){
+	$user = R::dispense('users');
+	$user ->name = $name;
+	R::store($user);
+	return "Пользователь $name зарегистрирован!";	
+}
 ?>
 
